b069932966f5332e3854c623cae4899acd74d3be
unknown <lweijler@cvl.tuwien.ac.at>
2024-04-18 22:50:58 +0200

    add if-name-main guard in viz.py for windows users

--- assignments/assignment_1/dlvc/viz.py
+++ assignments/assignment_1/dlvc/viz.py
@@ -16,26 +16,26 @@ def imshow(img):
     plt.imsave("test_1.png",np.transpose(npimg, (1, 2, 0)))
 
 
+if __name__ == '__main__': 
+    classes = ('plane', 'car', 'bird', 'cat',
+            'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
-classes = ('plane', 'car', 'bird', 'cat',
-           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
-
-transform = v2.Compose([v2.ToImage(), 
-                            v2.ToDtype(torch.float32, scale=True)])
+    transform = v2.Compose([v2.ToImage(), 
+                                v2.ToDtype(torch.float32, scale=True)])
 
 
-    
-train_data = CIFAR10Dataset(fdir="your_path_to_the_dataset", subset=Subset.TRAINING, transform=transform)
-train_data_loader = torch.utils.data.DataLoader(train_data,
-                                          batch_size=8,
-                                          shuffle=False,
-                                          num_workers=2)
+        
+    train_data = CIFAR10Dataset(fdir="your_path_to_the_dataset", subset=Subset.TRAINING, transform=transform)
+    train_data_loader = torch.utils.data.DataLoader(train_data,
+                                            batch_size=8,
+                                            shuffle=False,
+                                            num_workers=2)
 
-# get some random training images
-dataiter = iter(train_data_loader)
-images, labels = next(dataiter)
+    # get some random training images
+    dataiter = iter(train_data_loader)
+    images, labels = next(dataiter)
 
-# show images
-imshow(torchvision.utils.make_grid(images))
-# print labels
-print(' '.join(f'{classes[labels[j]]:5s}' for j in range(8)))
+    # show images
+    imshow(torchvision.utils.make_grid(images))
+    # print labels
+    print(' '.join(f'{classes[labels[j]]:5s}' for j in range(8)))
