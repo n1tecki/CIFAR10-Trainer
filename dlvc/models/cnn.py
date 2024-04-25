28a36ecd13069f419b0c5c37f3c6f7dff88eec15
n1tecki <mariusz.nitecki@aol.com>
2024-04-25 18:20:16 +0200

    Setting up ViC and CNN arhitectures and training code

--- dlvc/models/cnn.py
+++ dlvc/models/cnn.py
@@ -5,15 +5,20 @@ import torch
 
 
 
-
 class YourCNN(nn.Module):
     def __init__(self):
-        super().__init__()
-        ## TODO implement
-        pass
+        super(YourCNN, self).__init__()
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=5, stride=1, padding=2)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=5, stride=1, padding=2)
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
+        self.fc1 = nn.Linear(64 * 8 * 8, 256)
+        self.fc2 = nn.Linear(256, 128)
+        self.fc3 = nn.Linear(128, 10)
 
     def forward(self, x):
-        ## TODO implement
-        pass
-
-    
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 64 * 8 * 8)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        return F.log_softmax(self.fc3(x), dim=1)
